e4080e42465d947688853625f03e1692941f8100
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-05-03 14:23:34 -0400

    function practice

--- section 3/main.py
+++ section 3/main.py
@@ -1,17 +1,34 @@
-def findNextSquare(sq):
-    result = sq**.5
-    if result % 2 == 0 or result % 2 == 1:
-        next_square = (result + 1)**2
-        return f"the next square is {next_square}"
-
-    else:
-        return -1
-findNextSquare(37)
+from IPython.display import clear_output
 
 
+board = ["#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+game_on = True
+
+while game_on:
+
+    def display_board(board):
+        clear_output()
+        print("   |   |   ")
+        print(' ' + board[1] + ' ' + '|' + ' ' + board[2] + ' ' + '|' + ' ' + board[3] + ' ')
+        print("   |   |   ")
+        print("-----------")
+        print("   |   |   ")
+        print(' ' + board[4] + ' ' + '|' + ' ' + board[5] + ' ' + '|' + ' ' + board[6] + ' ')
+        print("   |   |   ")
+        print("-----------")
+        print("   |   |   ")
+        print(' ' + board[7] + ' ' + '|' + ' ' + board[8] + ' ' + '|' + ' ' + board[9] + ' ')
+        print("   |   |   ")
+
+    def player_input():
+        marker = " "
+        while marker != "X" and marker != "O":
+            marker = input("Player1: Choose X or O: ").upper()
+        if marker == "X":
+            return ("X", "O")
+        else:
+            return ("O", "X")
 
 
 
-
-
-
+    display_board(board)
