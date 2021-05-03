15598d8e1097f166865b8c0fd2765cdfd95b805a
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-05-03 11:49:42 -0400

    function practice

--- section 3/main.py
+++ section 3/main.py
@@ -1,19 +1,12 @@
 def findNextSquare(sq):
-    num_list = []
-    for num in range(1,sq):
-        num_list.append(num)
     result = sq**.5
-    if result in num_list:
-        i = num_list.index(result)
-        print(num_list)
-        print(i)
-        next_index = num_list[i]
-        next_square = num_list[next_index]**2
+    if result % 2 == 0 or result % 2 == 1:
+        next_square = (result + 1)**2
         return f"the next square is {next_square}"
 
     else:
         return -1
-findNextSquare(25)
+findNextSquare(37)
 
 
 
