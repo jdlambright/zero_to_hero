d1bfd428bf7d405fe5619022f367c126846c6dcc
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-05-08 21:20:02 -0400

    codewars problems

--- code_wars/even_index.py
+++ code_wars/even_index.py
@@ -1,24 +1,25 @@
 def find_even_index(arr):
-
-    for i in arr:
+    i = 0
+    equal_slices = False
+    while not equal_slices:
         left_slice = arr[:i]
         right_slice = arr[i + 1:]
         left_total = sum(left_slice)
         right_total = sum(right_slice)
 
         if left_total == right_total:
-            print(left_slice)
-            print(right_slice)
-            print(arr.index(i)+1)
-            return arr.index(i)+1
+            print(i)
+            equal_slices = True
+            return i
         else:
-            print(left_slice)
-            print(right_slice)
-            print(-1)
-            return -1
+            i+= 1
+
+        if i > len(arr):
+            if left_total != right_total:
+                break
+    print (-1)
+    return -1
 
 
-
-
-
-find_even_index([1,2,3,4,3,2,1])
+#find_even_index([1,2,3,4,3,2,1])
+find_even_index([20,10,30,10,40,10,15,35])
