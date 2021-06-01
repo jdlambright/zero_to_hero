bd9794a31b4e1a77591f9e60c8daffe0a0f0e529
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-06-01 15:29:12 -0400

    codewars problems

--- section 3/main.py
+++ section 3/main.py
@@ -1,5 +1,38 @@
-n = 1
+'''
+# Challenge:
+# Challenge:
+ATM machines allow 4 or 6 digit PIN codes and
+PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
-while n <=5:
-    print(n)
-    n+=1
+Create a function to validate whether an input string is a valid pin.
+
+# Examples:
+validate_pin("1234")   -->  True
+validate_pin("12345")  -->  False
+validate_pin("a234")   -->  False
+'''
+
+def valid_pin(pin):
+    valid_nums = ["1","2","3","4","5","6","7","8","9","0"]
+    pin.split()
+    if len(pin) == 4 or len(pin) == 6:
+        for char in pin:
+            if char in valid_nums:
+                continue
+            else:
+                print("false")
+                return False
+        else:
+            print("true")
+            return True
+    else:
+        print("false")
+        return False
+
+
+
+
+
+
+
+valid_pin("b23456")
