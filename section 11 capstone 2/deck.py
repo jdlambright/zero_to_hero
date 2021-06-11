a213f07f9d359c2cd13424a0008dc346b1859cd2
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-06-11 16:22:54 -0400

    codewars problems

--- section 11 capstone 2/deck.py
+++ section 11 capstone 2/deck.py
@@ -1,10 +1,26 @@
 from card import Card
+import random
 
 
+class Deck(Card):
 
-class Deck:
-    def __init__(self):
+    def __init__(self,rank, suit):
+        super().__init__(self, rank, suit)
 
         self.all_cards = []
 
-        for suit in Suits
+        for suit in SUITS:
+            for rank in RANKS:
+                #create teh card object
+                created_card = Card(rank,suit)
+                self.all_cards.append(created_card)
+
+
+    def shuffle(self):
+
+        random.shuffle(self.all_cards)
+
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
